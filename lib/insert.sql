cbd80b2d39e9fe50906b3165c23499edc8c42682
Alex Lee <hyunho98@gmail.com>
2020-02-02 01:11:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -25,12 +25,16 @@ INSERT INTO authors (name) VALUES ("J R R Tolkien");
 
 INSERT INTO character_books (book_id, character_id) VALUES (1, 1);
 INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 4);
 INSERT INTO character_books (book_id, character_id) VALUES (2, 3);
 INSERT INTO character_books (book_id, character_id) VALUES (2, 4);
 INSERT INTO character_books (book_id, character_id) VALUES (3, 3);
 INSERT INTO character_books (book_id, character_id) VALUES (3, 4);
 INSERT INTO character_books (book_id, character_id) VALUES (4, 5);
 INSERT INTO character_books (book_id, character_id) VALUES (4, 6);
+INSERT INTO character_books (book_id, character_id) VALUES (4, 7);
+INSERT INTO character_books (book_id, character_id) VALUES (4, 8);
 INSERT INTO character_books (book_id, character_id) VALUES (5, 7);
 INSERT INTO character_books (book_id, character_id) VALUES (5, 8);
 INSERT INTO character_books (book_id, character_id) VALUES (6, 7);
